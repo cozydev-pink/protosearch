3296ef72af12fa2f5d4cf8e8e1040d49c57b7bfc
Andrew Valencik <andrew.valencik@gmail.com>
2022-12-31 10:58:47 -0500

    Add TermIndexArray

---
+++ core/src/main/scala/io/pig/protosearch/TermIndexArray.scala
@@ -0,0 +1,121 @@
+package io.pig.protosearch
+
+import scala.annotation.tailrec
+import scala.collection.mutable.ArrayBuilder
+import scala.collection.mutable.ListBuffer
+
+sealed abstract class TermIndexArray private (
+    private val termDict: Array[String],
+    private val tfData: Array[Array[Int]],
+) {
+
+  val numTerms = termDict.size
+
+  def docCount(term: String): Int = {
+    val idx = termIndex(term)
+    if (idx < 0) 0
+    else tfData(idx).size / 2
+  }
+
+  def docsWithTerm(term: String): List[Int] = {
+    val idx = termIndex(term)
+    if (idx < 0) Nil
+    else evenElems(tfData(idx))
+  }
+
+  def docsWithTermTFIDF(term: String): List[(Int, Double)] = {
+    val idx = termIndex(term)
+    if (idx < 0) Nil
+    else {
+      val arr = tfData(idx)
+      var i = 0
+      val bldr = ListBuffer.newBuilder[(Int, Double)]
+      bldr.sizeHint(arr.size / 2)
+      while (i < arr.length) {
+        val id = arr(i)
+        val tf = Math.log(1.0 + arr(i + 1))
+        val idf: Double = 2.0 / arr.size.toDouble
+        val tfidf: Double = tf * idf
+        // println(s"tf: $tf, idf: $idf, tfidf: $tfidf")
+        bldr += (id -> tfidf)
+        i += 2
+      }
+      bldr.result().sortBy(-_._2).toList
+    }
+  }
+
+  private def termIndex(term: String): Int =
+    binarySearch(term, 0, numTerms)
+
+  @tailrec
+  private def binarySearch(elem: String, from: Int, to: Int): Int =
+    if (to <= from) -1 // term doesn't exist, prefix search should start around here
+    else {
+      val idx = from + (to - from - 1) / 2
+      math.signum(elem.compareTo(termDict(idx))) match {
+        case -1 => binarySearch(elem, from, idx)
+        case 1 => binarySearch(elem, idx + 1, to)
+        case _ => idx
+      }
+    }
+
+  private def evenElems(arr: Array[Int]): List[Int] = {
+    require(
+      arr.size >= 2 && arr.size % 2 == 0,
+      "evenElems expects even sized arrays of 2 or greater",
+    )
+    if (arr.size == 2) arr(0) :: Nil
+    else {
+      var i = 0
+      val bldr = ListBuffer.newBuilder[Int]
+      bldr.sizeHint(arr.size / 2)
+      while (i < arr.length) {
+        bldr += arr(i)
+        i += 2
+      }
+      bldr.result().toList
+    }
+  }
+
+}
+object TermIndexArray {
+  import scala.collection.mutable.{TreeMap => MMap}
+  import scala.collection.mutable.Stack
+
+  def apply(docs: List[List[String]]): TermIndexArray = {
+    val m = new MMap[String, Stack[Int]].empty
+    var docId = 0
+    val docLen = docs.length
+    while (docId < docLen) {
+      docs(docId).foreach { term =>
+        val s = m.getOrElseUpdate(term, Stack.empty)
+        if (s.isEmpty) {
+          // println(s"doc($docId), term($term), init freq = 1")
+          s.prepend(1).prepend(docId)
+        } else {
+          val lastDoc = s.pop()
+          val freq = s.pop()
+          if (lastDoc == docId) {
+            //   println(s"doc($docId), term($term), INCREMENT newFreq=${freq + 1}")
+            s.prepend(freq + 1).prepend(docId)
+          } else {
+            //   println(s"doc($docId), term($term) new doc! init freq = 1")
+            s.prepend(freq).prepend(lastDoc).prepend(1).prepend(docId)
+          }
+        }
+      }
+      docId += 1
+    }
+    val keys = ArrayBuilder.make[String]
+    val values = ArrayBuilder.make[Array[Int]]
+    val size = m.size
+    keys.sizeHint(size)
+    values.sizeHint(size)
+    m.foreachEntry { (k, v) =>
+      keys.addOne(k)
+      values.addOne(v.toArray)
+    }
+    // val arr = m.view.mapValues(_.toArray).toArray.sortInPlaceBy(_._1)
+    new TermIndexArray(keys.result(), values.result()) {}
+  }
+}
---
+++ core/src/test/scala/io/pig/protosearch/TermIndexArraySuite.scala
@@ -0,0 +1,52 @@
+package io.pig.protosearch
+
+class TermIndexArraySuite extends munit.FunSuite {
+  def tokenize(s: String): List[String] =
+    s.split(" ").toList
+
+  val docs = List(
+    tokenize("the quick brown fox jumped over the lazy cat"),
+    tokenize("the very fast cat jumped across the room"),
+    tokenize("a lazy cat sleeps all day"),
+  )
+
+  lazy val index = TermIndexArray(docs)
+
+  test("apply builds from list of lists of strings") {
+    assertEquals(index.numTerms, 16)
+  }
+
+  test("docCount returns zero when no docs contain term") {
+    assertEquals(index.docCount("???"), 0)
+  }
+
+  test("docCount returns number of documents containing term") {
+    assertEquals(index.docCount("cat"), 3)
+    assertEquals(index.docCount("lazy"), 2)
+    assertEquals(index.docCount("room"), 1)
+  }
+
+  test("docsWithTerm returns empty list when no docs contain term") {
+    assertEquals(index.docsWithTerm("???"), Nil)
+  }
+
+  test("docsWithTerm returns list of docIDs containing term") {
+    assertEquals(index.docsWithTerm("cat").sorted, List(0, 1, 2))
+    assertEquals(index.docsWithTerm("the").sorted, List(0, 1))
+    assertEquals(index.docsWithTerm("lazy").sorted, List(0, 2))
+  }
+
+  test("docsWithTermTFIDF returns list of docIDs and tf-idf scores") {
+    val samples = List(
+      tokenize("this is a sample"),
+      tokenize("this is another example"),
+    )
+    val index = TermIndexArray(samples)
+    assertEquals(index.docsWithTermTFIDF("example").sorted, List((1, 0.6931471805599453)))
+  }
+
+  test("docsWithTermTFIDF returns list of docIDs containing term in order of TFIDF") {
+    assertEquals(index.docsWithTermTFIDF("lazy").map(_._1), List(2, 0))
+  }
+
+}
