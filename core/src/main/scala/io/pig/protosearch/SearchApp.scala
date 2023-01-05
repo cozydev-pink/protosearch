b3c8ed8237ff22c3f6a39db713cb212f6ba9ae4c
Andrew Valencik <andrew.valencik@gmail.com>
2023-01-05 08:49:40 -0500

    Add search query to SearchApp

--- core/src/main/scala/io/pig/protosearch/SearchApp.scala
+++ core/src/main/scala/io/pig/protosearch/SearchApp.scala
@@ -23,12 +23,20 @@ object SearchApp extends IOApp.Simple {
   val bytes = Codec.termIndex.encode(index.tfData, index.termDict)
   val bytesLog = IO.println(s"Encoded to ${bytes.toOption.get.size} bytes")
 
-  val decIndex = bytes.flatMap(Codec.termIndex.decode)
+  val decIndex = bytes.flatMap(Codec.termIndex.decodeValue)
   val decLog = decIndex match {
     case Failure(cause) => IO.println(s"failed to encode-decode with error: $cause")
     case Successful(value) => IO.println(s"encoded-decoded vector: ${value}")
   }
 
-  val run = indexLog *> bytesLog *> decLog *> IO.println("- FIN -")
+  val indexIO = IO.fromEither(decIndex.toEither.left.map(e => new Throwable(e.message)))
+  def search(query: String) = {
+    val q = tokenize(query)
+    indexIO.flatMap(index => IO.println(q.map(index.docsWithTermTFIDF).mkString("\n")))
+  }
+
+  val query = "fast"
+
+  val run = indexLog *> bytesLog *> decLog *> search(query) *> IO.println("- FIN -")
 
 }
