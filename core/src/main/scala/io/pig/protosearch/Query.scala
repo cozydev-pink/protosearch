ea47a58e410a0f6c17bc2e6d4912fb65993a4cf9
Andrew Valencik <andrew.valencik@gmail.com>
2023-01-07 12:21:35 -0500

    Separate getting docs from scoring

--- core/src/main/scala/io/pig/protosearch/Query.scala
+++ core/src/main/scala/io/pig/protosearch/Query.scala
@@ -1,27 +1,38 @@
 package io.pig.protosearch
 
 import cats.data.NonEmptyList
+import cats.syntax.all._
 import io.pig.lucille.Query
 
 object BooleanQuery {
 
-  def search(index: TermIndexArray, q: Query): Either[String, List[(Int, Double)]] =
+  def search(index: TermIndexArray, q: Query): Either[String, List[(Int, Double)]] = {
+    val terms = onlyTerms(NonEmptyList.of(q))
+    val docs = booleanModel(index, q)
+    terms.flatMap(terms => docs.map(ds => BooleanQueryImpl.scoreEm(index, terms, ds)))
+  }
+
+  def booleanModel(index: TermIndexArray, q: Query): Either[String, Set[Int]] =
     q match {
       case Query.OrQ(qs) => onlyTerms(qs).map(qs => BooleanQueryImpl.OrQ(qs).search(index))
       case Query.AndQ(qs) => onlyTerms(qs).map(qs => BooleanQueryImpl.AndQ(qs).search(index))
       case Query.TermQ(q) => Right(BooleanQueryImpl.termQ(index, q))
+      case Query.PhraseQ(_) => Left("Phrase queries require position data, which we don't have yet")
+      case _: Query.FieldQ => Left("We only have one implicit field currently")
       case _ => Left("Bro, c'mon, only ORs and ANDs, thank you")
     }
 
   private def onlyTerms(qs: NonEmptyList[Query]): Either[String, NonEmptyList[String]] =
-    qs.traverse {
-      case t: Query.TermQ => Right(t.q)
-      case _ => Left("Sorry bucko, only term queries supported today")
+    qs.flatTraverse {
+      case Query.OrQ(qs) => onlyTerms(qs)
+      case Query.AndQ(qs) => onlyTerms(qs)
+      case Query.TermQ(t) => Right(NonEmptyList.of(t))
+      case x => Left(s"Sorry bucko, only term queries supported today, not $x")
     }
 }
 
 object BooleanQueryImpl {
-  private def scoreEm(
+  def scoreEm(
       index: TermIndexArray,
       terms: NonEmptyList[String],
       docs: Set[Int],
@@ -31,24 +42,16 @@ object BooleanQueryImpl {
       .groupMapReduce(_._1)(_._2)(_ + _)
       .toList
 
-  def termQ(index: TermIndexArray, t: String): List[(Int, Double)] = {
-    val docs: Set[Int] = index.docsWithTermSet(t)
-    scoreEm(index, NonEmptyList.of(t), docs)
-  }
+  def termQ(index: TermIndexArray, t: String): Set[Int] =
+    index.docsWithTermSet(t)
 
   case class OrQ(terms: NonEmptyList[String]) {
-
-    def search(index: TermIndexArray): List[(Int, Double)] = {
-      val docs: Set[Int] = terms.toList.map(t => index.docsWithTermSet(t)).reduce(_ union _)
-      scoreEm(index, terms, docs)
-    }
+    def search(index: TermIndexArray): Set[Int] =
+      terms.toList.map(t => index.docsWithTermSet(t)).reduce(_ union _)
   }
 
   case class AndQ(terms: NonEmptyList[String]) {
-
-    def search(index: TermIndexArray): List[(Int, Double)] = {
-      val docs: Set[Int] = terms.toList.map(t => index.docsWithTermSet(t)).reduce(_ intersect _)
-      scoreEm(index, terms, docs)
-    }
+    def search(index: TermIndexArray): Set[Int] =
+      terms.toList.map(t => index.docsWithTermSet(t)).reduce(_ intersect _)
   }
 }
