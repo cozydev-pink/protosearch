46e85c70b6c398d84ed87dab9c15f5564a225bd3
Andrew Valencik <andrew.valencik@gmail.com>
2023-03-01 20:23:19 -0500

    Add quick TermIndexCodec round trip test

---
+++ core/src/test/scala/pink/cozydev/protosearch/TermIndexCodecSuite.scala
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2022 CozyDev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pink.cozydev.protosearch
+
+class TermIndexCodecSuite extends munit.FunSuite {
+  val index = CatIndex.index
+
+  test("TermIndexCodec.termIndex encodes") {
+    val bytes = TermIndexCodec.termIndex.encode(index)
+    assert(bytes.isSuccessful)
+  }
+
+  test("TermIndexCodec.termIndex round trips") {
+    val bytes = TermIndexCodec.termIndex.encode(index)
+    val indexDecoded = bytes.flatMap(TermIndexCodec.termIndex.decodeValue)
+    assert(indexDecoded.isSuccessful)
+  }
+
+}
