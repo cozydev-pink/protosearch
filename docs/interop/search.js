a19804c509007aa7f9c46ed78ee68cfdfe9b8474
Andrew Valencik <andrew.valencik@gmail.com>
2023-06-03 08:35:35 -0400

    Print titles and simplified scores

--- docs/interop/search.js
+++ docs/interop/search.js
@@ -1,16 +1,31 @@
-var app = document.getElementById("app")
-var list = ''
-
-function searchIt(querier) {
-  querier.search("client").forEach(h => {
-    list += '<li>' + h.id + ' score: ' + h.score + '</li>'
-  })
-  app.innerHTML += list
-
+async function getDocs() {
+  let docs = fetch("/searchdocs/http4s-docs.json")
+    .then(res => res.json())
+    .catch((error) => console.error(error));
+  return await docs
 }
 
-let querier = fetch("/searchdocs/http4s-docs.idx")
-  .then(res => res.blob())
-  .then(blob => QuerierBuilder.load(blob, "body"))
-  .then(querier => searchIt(querier))
-  .catch((error) => console.error(error));
+async function getQuerier() {
+  let querier = fetch("/searchdocs/http4s-docs.idx")
+    .then(res => res.blob())
+    .then(blob => QuerierBuilder.load(blob, "body"))
+    .catch((error) => console.error(error));
+  return await querier
+}
+
+function searchIt(docs, querier) {
+  var list = ''
+  querier.search("client").forEach(h => {
+    const title = docs[h.id].title
+    const score = parseInt(h.score*1000)
+    list += '<li>' + title + ' score: ' + score + '</li>'
+  })
+  return list
+}
+
+async function main() {
+  let [docs, querier] = await Promise.all([getDocs(), getQuerier()])
+  var app = document.getElementById("app")
+  app.innerHTML += searchIt(docs, querier)
+}
+main()
