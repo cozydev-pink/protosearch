c47e389d03b5f9f1735a5100bbae302f21803535
Andrew Valencik <andrew.valencik@gmail.com>
2023-12-07 18:40:46 -0500

    Remove unused searchIt function from search.js

--- sbt/src/main/resources/pink/cozydev/protosearch/sbt/search.js
+++ sbt/src/main/resources/pink/cozydev/protosearch/sbt/search.js
@@ -1,15 +1,3 @@
-
-
-function searchIt(query, docs, querier) {
-  var list = ''
-  querier.searchPrefix(query).forEach(h => {
-    const title = docs[h.id].title
-    const score = parseInt(h.score*1000)
-    list += '<li>' + title + ' score: ' + score + '</li>'
-  })
-  return list
-}
-
 async function main() {
   var app = document.getElementById("app")
   var searchBar = document.getElementById("search_input")
