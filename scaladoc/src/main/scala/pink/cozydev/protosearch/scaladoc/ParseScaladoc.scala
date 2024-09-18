74b681291ef9298787fc97f36f2eaa79fd9955ca
golden-skipper03 <157791077+golden-skipper03@users.noreply.github.com>
2024-09-18 19:10:10 +0530

    format-code

--- scaladoc/src/main/scala/pink/cozydev/protosearch/scaladoc/ParseScaladoc.scala
+++ scaladoc/src/main/scala/pink/cozydev/protosearch/scaladoc/ParseScaladoc.scala
@@ -6,16 +6,15 @@ import scala.meta.contrib.DocToken._
 import scala.meta.tokens.Token.Comment
 import scala.util.matching.Regex
 
-
 case class ScaladocInfo(
-  name: String,
-  description: String,
-  annotations: List[String],
-  hyperlinks: List[String],
-  params  : List[String],
-  returnType: String,
-  startLine: Int,
-  endLine: Int
+    name: String,
+    description: String,
+    annotations: List[String],
+    hyperlinks: List[String],
+    params: List[String],
+    returnType: String,
+    startLine: Int,
+    endLine: Int,
 )
 
 object ParseScaladoc {
@@ -33,115 +32,125 @@ object ParseScaladoc {
 
     val functions = parsed.collect {
       case defn @ Defn.Def.After_4_7_3(_, name, paramss, retType, _) =>
-      val (commentTokens, rawComment): (List[DocToken], String) = comments
-      .filter { case (start, _) => start < defn.pos.start }
-      .toList
-      .sortBy(_._1)
-      .reverse.headOption
-      .map { case (_, tokens) =>
-        val raw = tokens.collect {
-          case DocToken(_, _, Some(body)) => body
-        }.mkString(" ")
-        (tokens, raw)
-      }
-      .getOrElse((Nil, ""))
+        val (commentTokens, rawComment): (List[DocToken], String) = comments
+          .filter { case (start, _) => start < defn.pos.start }
+          .toList
+          .sortBy(_._1)
+          .reverse
+          .headOption
+          .map { case (_, tokens) =>
+            val raw = tokens
+              .collect { case DocToken(_, _, Some(body)) =>
+                body
+              }
+              .mkString(" ")
+            (tokens, raw)
+          }
+          .getOrElse((Nil, ""))
 
-      val startLine = defn.pos.startLine
-      val endLine = defn.pos.endLine  
+        val startLine = defn.pos.startLine
+        val endLine = defn.pos.endLine
 
-      val description = commentTokens.collect {
-        case DocToken(Description, _, Some(body)) => body
-      }.mkString(" ")
+        val description = commentTokens
+          .collect { case DocToken(Description, _, Some(body)) =>
+            body
+          }
+          .mkString(" ")
 
-      val paramsComm  = commentTokens.collect {
-        case DocToken(Param, Some(name), Some(desc)) => s"$name: $desc"
-      }
+        val paramsComm = commentTokens.collect { case DocToken(Param, Some(name), Some(desc)) =>
+          s"$name: $desc"
+        }
 
-      val params = paramss.flatMap {
-        case Member.ParamClauseGroup(_,params) => params.flatMap {
-          case clause: Member.ParamClause =>
+        val params = paramss.flatMap { case Member.ParamClauseGroup(_, params) =>
+          params.flatMap { case clause: Member.ParamClause =>
             clause.values.collect {
-              case param: Term.Param if param.default.isEmpty && !param.mods.exists(_.is[Mod.Implicit]) =>
-                val commentDescription: String = paramsComm.find(_.startsWith(param.name.value))
+              case param: Term.Param
+                  if param.default.isEmpty && !param.mods.exists(_.is[Mod.Implicit]) =>
+                val commentDescription: String = paramsComm
+                  .find(_.startsWith(param.name.value))
                   .getOrElse(param.name.value)
                 val declaredType: String = param.decltpe.map(_.toString).getOrElse("Unknown Type")
 
                 s"$commentDescription: $declaredType"
             }
-        }
-      }
-      
-
-      val typeParamsComm = commentTokens.collect {
-        case DocToken(TypeParam, Some(name), Some(desc)) => s"@tparam $name: $desc"
-      }
-
-      val typeParamRegex = """def\s+\w+\[([^\]]+)\]""".r
-
-      val typeParams = typeParamRegex.findFirstMatchIn(defn.syntax) match {
-        case Some(matched) =>
-          matched.group(1).split(",").map { tparam =>
-            val trimmedTparam = tparam.trim
-            val commentDescription = typeParamsComm.find(_.startsWith(s"@tparam $trimmedTparam"))
-              .getOrElse(trimmedTparam)
-            commentDescription.replace("@tparam"+" "+trimmedTparam, trimmedTparam)
-          }.toList
-
-        case None => List()
-      }
-
-      val annotations = defn.mods.collect {
-        case mod: Mod.Annot => mod.toString
-      }
-
-      val hyperlinks = urlRegex.findAllMatchIn(rawComment).map(_.matched).toList
-
-      val optionalParams = paramss.flatMap {
-        case Member.ParamClauseGroup(_,params) => params.flatMap {
-          case clause: Member.ParamClause =>
-            clause.values.collect {
-            case param: Term.Param if param.default.isDefined =>
-              val commentDescription: String = paramsComm.find(_.startsWith(param.name.value))
-                .getOrElse(param.name.value)
-              val declaredType: String = param.decltpe.map(_.toString).getOrElse("Unknown Type")
-
-              s"$commentDescription: $declaredType"
           }
         }
-      }
 
-      val implicitParams = paramss.flatMap {
-        case Member.ParamClauseGroup(_,params) => params.flatMap {
-          case clause: Member.ParamClause => clause.values.collect {
-              case param: Term.Param if param.mods.exists(_.is[Mod.Implicit]) =>
-                val commentDescription: String = paramsComm.find(_.startsWith(param.name.value))
+        val typeParamsComm = commentTokens.collect {
+          case DocToken(TypeParam, Some(name), Some(desc)) => s"@tparam $name: $desc"
+        }
+
+        val typeParamRegex = """def\s+\w+\[([^\]]+)\]""".r
+
+        val typeParams = typeParamRegex.findFirstMatchIn(defn.syntax) match {
+          case Some(matched) =>
+            matched
+              .group(1)
+              .split(",")
+              .map { tparam =>
+                val trimmedTparam = tparam.trim
+                val commentDescription = typeParamsComm
+                  .find(_.startsWith(s"@tparam $trimmedTparam"))
+                  .getOrElse(trimmedTparam)
+                commentDescription.replace("@tparam" + " " + trimmedTparam, trimmedTparam)
+              }
+              .toList
+
+          case None => List()
+        }
+
+        val annotations = defn.mods.collect { case mod: Mod.Annot =>
+          mod.toString
+        }
+
+        val hyperlinks = urlRegex.findAllMatchIn(rawComment).map(_.matched).toList
+
+        val optionalParams = paramss.flatMap { case Member.ParamClauseGroup(_, params) =>
+          params.flatMap { case clause: Member.ParamClause =>
+            clause.values.collect {
+              case param: Term.Param if param.default.isDefined =>
+                val commentDescription: String = paramsComm
+                  .find(_.startsWith(param.name.value))
                   .getOrElse(param.name.value)
                 val declaredType: String = param.decltpe.map(_.toString).getOrElse("Unknown Type")
 
                 s"$commentDescription: $declaredType"
             }
+          }
         }
-      }
 
-      val allParams = typeParams ++ params ++ optionalParams ++ implicitParams 
+        val implicitParams = paramss.flatMap { case Member.ParamClauseGroup(_, params) =>
+          params.flatMap { case clause: Member.ParamClause =>
+            clause.values.collect {
+              case param: Term.Param if param.mods.exists(_.is[Mod.Implicit]) =>
+                val commentDescription: String = paramsComm
+                  .find(_.startsWith(param.name.value))
+                  .getOrElse(param.name.value)
+                val declaredType: String = param.decltpe.map(_.toString).getOrElse("Unknown Type")
 
-      val returnType = retType match {
-        case Some(tpe) => tpe.syntax
-        case None => "Unit"
-      }
+                s"$commentDescription: $declaredType"
+            }
+          }
+        }
 
-      ScaladocInfo(
-        name.value,
-        description , 
-        annotations,
-        hyperlinks ,
-        allParams,
-        returnType,
-        startLine,
-        endLine
-      )
+        val allParams = typeParams ++ params ++ optionalParams ++ implicitParams
+
+        val returnType = retType match {
+          case Some(tpe) => tpe.syntax
+          case None => "Unit"
+        }
+
+        ScaladocInfo(
+          name.value,
+          description,
+          annotations,
+          hyperlinks,
+          allParams,
+          returnType,
+          startLine,
+          endLine,
+        )
     }
-
     functions
   }
 }
--- scaladoc/src/main/scala/pink/cozydev/protosearch/scaladoc/ScaladocSearcher.scala
+++ scaladoc/src/main/scala/pink/cozydev/protosearch/scaladoc/ScaladocSearcher.scala
@@ -1,14 +1,14 @@
 package pink.cozydev.protosearch.scaladoc
 
 object ScaladocSearcher {
-  def searchScaladoc(source: String,queries: List[String]) : List[ScaladocInfo] = {
+  def searchScaladoc(source: String, queries: List[String]): List[ScaladocInfo] = {
     val scaladocInfoList = ParseScaladoc.parseAndExtractInfo(source)
     val index = ScaladocIndexer.createScaladocIndex(scaladocInfoList)
     def search(q: String): List[ScaladocInfo] = {
-        val searchResults = index.search(q)
-        searchResults.fold(_ => Nil, hits => hits.map(h => scaladocInfoList.toList(h.id)))
+      val searchResults = index.search(q)
+      searchResults.fold(_ => Nil, hits => hits.map(h => scaladocInfoList.toList(h.id)))
     }
-    
+
     val results: List[ScaladocInfo] = queries.flatMap { query =>
       search(query)
     }
