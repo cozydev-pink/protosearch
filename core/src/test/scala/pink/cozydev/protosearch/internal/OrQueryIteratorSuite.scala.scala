554a4d098fbfa2f6f9c68ea6b11f1583fa9d42f7
Andrew Valencik <andrew.valencik@gmail.com>
2025-03-30 08:32:50 -0400

    Improved OrQueryIteratorSuite test suite

--- core/src/test/scala/pink/cozydev/protosearch/internal/OrQueryIteratorSuite.scala.scala
+++ core/src/test/scala/pink/cozydev/protosearch/internal/OrQueryIteratorSuite.scala.scala
@@ -16,129 +16,125 @@ class OrQueryIteratorSuite extends munit.FunSuite {
     )
   )
 
-  def exact(phrase: String): PositionalIter =
-    PositionalIter.exact(index, Query.Phrase(phrase)).get
+  // Iters are stateful, so we make some constructors that return "thunks"
+  case class TestQuery(label: String, iter: () => QueryIterator)
+  object TestQuery {
+    def noMatch: TestQuery = TestQuery("noMatch", () => new NoMatchQueryIterator)
+    def exact(label: String, query: String): TestQuery =
+      TestQuery(
+        label,
+        () => positionalIter(query),
+      )
 
-  val matchNone = NoMatchQueryIterator
-
-  test("always match with 1 matching, minShouldMatch=1") {
-    val queries = Map(
-      "matchAB" -> exact("0 1 2 3"),
-      "noMatch1" -> matchNone,
-      "noMatch2" -> matchNone,
-    )
-    queries.toList.permutations.foreach { label_qis =>
-      val (labels, qis) = label_qis.unzip
-      val name = labels.mkString(", ")
-      val iter = OrQueryIterator(qis.toArray, 1)
-      val docs = iter.docs.toList
-      assertEquals(docs, List(1), clue = name)
-    }
+    private def positionalIter(q: String): PositionalIter =
+      PositionalIter.exact(index, Query.Phrase(q)) match {
+        case Some(iter) => iter
+        case None =>
+          val msg = s"some terms in query: '$q', could not be found in test index"
+          throw new IllegalArgumentException(msg)
+      }
   }
 
-  test("always match with 1 matching, minShouldMatch=1") {
-    val queries = Map(
-      "matchAB" -> exact("a b c d"),
-      "noMatch1" -> matchNone,
-      "noMatch2" -> matchNone,
-    )
-    queries.toList.permutations.foreach { label_qis =>
-      val (labels, qis) = label_qis.unzip
-      val name = labels.mkString(", ")
-      val iter = OrQueryIterator(qis.toArray, 1)
+  // The order of QueryIterators should not matter, so we test all permutations
+  def checkAllPermutations(
+      queries: List[TestQuery],
+      expected: List[Int],
+      minShouldMatch: Int,
+  )(implicit
+      loc: munit.Location
+  ): Unit =
+    queries.permutations.foreach { tqs =>
+      val name = tqs.map(_.label).mkString(", ")
+      println(s"--- testing: $name")
+      val iter = OrQueryIterator(tqs.map(tq => tq.iter()).toArray, minShouldMatch)
       val docs = iter.docs.toList
-      assertEquals(docs, List(2), clue = name)
+      println(s"--- result : $docs")
+      assertEquals(docs, expected, clue = name)
     }
+
+  test("always match with 1 matching, minShouldMatch=1 (doc1)") {
+    val queries = List(
+      TestQuery.exact("doc1", "0 1 2 3"),
+      TestQuery.noMatch,
+      TestQuery.noMatch,
+    )
+    checkAllPermutations(queries, List(1), minShouldMatch = 1)
   }
 
-  test("never match with 1 matching, minShouldMatch=2") {
-    val queries = Map(
-      "matchAB" -> exact("a b c d"),
-      "noMatch1" -> matchNone,
-      "noMatch2" -> matchNone,
+  test("always match with 1 matching, minShouldMatch=1 (doc2)") {
+    val queries = List(
+      TestQuery.exact("doc2", "a b c d"),
+      TestQuery.noMatch,
+      TestQuery.noMatch,
     )
-    queries.toList.permutations.foreach { label_qis =>
-      val (labels, qis) = label_qis.unzip
-      val name = labels.mkString(", ")
-      val iter = OrQueryIterator(qis.toArray, 2)
-      val docs = iter.docs.toList
-      assertEquals(docs, List.empty, clue = name)
-    }
+    checkAllPermutations(queries, List(2), minShouldMatch = 1)
   }
 
-  test("always match with 2 matching, minShouldMatch=1") {
-    val queries = Map(
-      "matchAB" -> exact("a b c d"),
-      "match01" -> exact("0 1 2 3"),
-      "noMatch1" -> matchNone,
-      "noMatch2" -> matchNone,
+  test("always match with 1 matching, minShouldMatch=1 (last doc)") {
+    assert(index.numDocs == 4, clue = "expected 4 to be last docId")
+    val queries = List(
+      TestQuery.exact("doc4", "0 b 2 d"),
+      TestQuery.noMatch,
+      TestQuery.noMatch,
     )
-    queries.toList.permutations.foreach { label_qis =>
-      val (labels, qis) = label_qis.unzip
-      val name = labels.mkString(", ")
-      val iter = OrQueryIterator(qis.toArray, 1)
-      val docs = iter.docs.toList
-      assertEquals(docs, List(1, 2), clue = name)
-    }
+    checkAllPermutations(queries, List(4), minShouldMatch = 1)
+  }
+
+  test("never match with 1 matching, minShouldMatch=2 (doc1)") {
+    val queries = List(
+      TestQuery.exact("doc1", "0 1 2 3"),
+      TestQuery.noMatch,
+      TestQuery.noMatch,
+    )
+    checkAllPermutations(queries, List.empty, minShouldMatch = 2)
+  }
+
+  test("never match with 1 matching, minShouldMatch=2 (doc2)") {
+    val queries = List(
+      TestQuery.exact("doc2", "a b c d"),
+      TestQuery.noMatch,
+      TestQuery.noMatch,
+    )
+    checkAllPermutations(queries, List.empty, minShouldMatch = 2)
+  }
+
+  test("never match with 1 matching, minShouldMatch=2 (last doc)") {
+    assert(index.numDocs == 4, clue = "expected 4 to be last docId")
+    val queries = List(
+      TestQuery.exact("doc4", "0 b 2 d"),
+      TestQuery.noMatch,
+      TestQuery.noMatch,
+    )
+    checkAllPermutations(queries, List.empty, minShouldMatch = 2)
+  }
+
+  test("always match with 2 matching, minShouldMatch=1".only) {
+    val queries = List(
+      TestQuery.exact("doc1", "0 1 2 3"),
+      TestQuery.exact("doc2", "a b c d"),
+      TestQuery.noMatch,
+      TestQuery.noMatch,
+    )
+    checkAllPermutations(queries, List(1, 2), minShouldMatch = 1)
   }
 
   test("always match with 2 matching, minShouldMatch=2") {
-    val queries = Map(
-      "matchAB" -> exact("a b c d"),
-      "match01" -> exact("0 1 2 3"),
-      "noMatch1" -> matchNone,
-      "noMatch2" -> matchNone,
+    val queries = List(
+      TestQuery.exact("doc1", "0 1 2 3"),
+      TestQuery.exact("doc2", "a b c d"),
+      TestQuery.noMatch,
+      TestQuery.noMatch,
     )
-    queries.toList.permutations.foreach { label_qis =>
-      val (labels, qis) = label_qis.unzip
-      val name = labels.mkString(", ")
-      val iter = OrQueryIterator(qis.toArray, 2)
-      val docs = iter.docs.toList
-      assertEquals(docs, List(1, 2), clue = name)
-    }
+    checkAllPermutations(queries, List(1, 2), minShouldMatch = 2)
   }
 
   test("never match with 2 matching, minShouldMatch=3") {
-    val queries = Map(
-      "matchAB" -> exact("a b c d"),
-      "match01" -> exact("0 1 2 3"),
-      "noMatch1" -> matchNone,
-      "noMatch2" -> matchNone,
+    val queries = List(
+      TestQuery.exact("doc1", "0 1 2 3"),
+      TestQuery.exact("doc2", "a b c d"),
+      TestQuery.noMatch,
+      TestQuery.noMatch,
     )
-    queries.toList.permutations.foreach { label_qis =>
-      val (labels, qis) = label_qis.unzip
-      val name = labels.mkString(", ")
-      val iter = OrQueryIterator(qis.toArray, 3)
-      val docs = iter.docs.toList
-      assertEquals(docs, List.empty, clue = name)
-    }
+    checkAllPermutations(queries, List.empty, minShouldMatch = 3)
   }
-
-//   test("do it") {
-//     val iter1 = PositionalIter.exact(index, Query.Phrase("very fast cat")).get
-//     val iter2 = PositionalIter.exact(index, Query.Phrase("jumped")).get
-//     val iter3 = PositionalIter.exact(index, Query.Phrase("cat")).get
-//     val iter4 = PositionalIter.exact(index, Query.Phrase("the room")).get
-//     val iter = new OrQueryIterator(Array(iter1, iter2, iter3, iter4), 1)
-//     val docs = iter.takeWhile(_ > -1).toSet
-//     assertEquals(docs, Set(1, 2))
-//   }
-
-//   test("do it") {
-//     val iter1 = PositionalIter.exact(index, Query.Phrase("very fast cat")).get
-//     val iter2 = PositionalIter.exact(index, Query.Phrase("jumped")).get
-//     val iter3 = PositionalIter.exact(index, Query.Phrase("cat")).get
-//     val iter4 = PositionalIter.exact(index, Query.Phrase("the room")).get
-//     val iter = new OrQueryIterator(Array(iter1, iter2, iter3, iter4), 3)
-//     val docs = iter.takeWhile(_ > -1).toSet
-//     assertEquals(docs, Set(1))
-//   }
-
-//   test("do it") {
-//     val iter1 = PositionalIter.exact(index, Query.Phrase("very fast cat")).get
-//     val iter2 = PositionalIter.exact(index, Query.Phrase("day")).get
-//     val iter = new OrQueryIterator(Array(iter1, iter2), 2)
-//     val docs = iter.takeWhile(_ > -1).toSet
-//     assert(docs.isEmpty)
-//   }
 }
