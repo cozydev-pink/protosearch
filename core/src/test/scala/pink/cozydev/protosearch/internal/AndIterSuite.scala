41ef6ea9c7f2923848883f98f26cbdf2460c2f6e
Andrew Valencik <andrew.valencik@gmail.com>
2025-03-30 11:50:55 -0400

    Use TestQueryFixtures in AndIterSuite

--- core/src/test/scala/pink/cozydev/protosearch/internal/AndIterSuite.scala
+++ core/src/test/scala/pink/cozydev/protosearch/internal/AndIterSuite.scala
@@ -16,37 +16,87 @@
 
 package pink.cozydev.protosearch.internal
 
-import pink.cozydev.lucille.Query
-import pink.cozydev.protosearch.fixtures.CatIndex
-import pink.cozydev.protosearch.PositionalIndex
+import TestQueryFixtures._
 
 class AndIterSuite extends munit.FunSuite {
 
-  val index = PositionalIndex(CatIndex.docs)
+  // The order of QueryIterators should not matter, so we test all permutations
+  def checkAllPermutations(
+      queries: List[TestQuery],
+      expected: List[Int],
+  )(implicit
+      loc: munit.Location
+  ): Unit =
+    queries.permutations.foreach { tqs =>
+      val name = tqs.map(_.label).mkString(", ")
+      val iter = new AndIter(tqs.map(tq => tq.iter()).toArray)
+      val docs = iter.docs.toList
+      assertEquals(docs, expected, clue = name)
+    }
 
-  test("do it") {
-    val iter1 = PositionalIter.exact(index, Query.Phrase("very fast cat")).get
-    val iter2 = PositionalIter.exact(index, Query.Phrase("the room")).get
-    val iter = new AndIter(Array(iter1, iter2))
-    val docs = iter.takeWhile(_ > -1).toSet
-    assertEquals(docs, Set(2))
+  test("never match with 1 noMatch (doc1)") {
+    val queries = List(
+      TestQuery.exact("doc1", "0 1 2 3"),
+      TestQuery.noMatch,
+    )
+    checkAllPermutations(queries, List.empty)
   }
 
-  test("do it") {
-    val iter1 = PositionalIter.exact(index, Query.Phrase("very fast cat")).get
-    val iter2 = PositionalIter.exact(index, Query.Phrase("jumped")).get
-    val iter3 = PositionalIter.exact(index, Query.Phrase("cat")).get
-    val iter4 = PositionalIter.exact(index, Query.Phrase("the room")).get
-    val iter = new AndIter(Array(iter1, iter2, iter3, iter4))
-    val docs = iter.takeWhile(_ > -1).toSet
-    assertEquals(docs, Set(2))
+  test("never match with 1 noMatch (doc2)") {
+    val queries = List(
+      TestQuery.exact("doc2", "a b c d"),
+      TestQuery.noMatch,
+    )
+    checkAllPermutations(queries, List.empty)
   }
 
-  test("do it") {
-    val iter1 = PositionalIter.exact(index, Query.Phrase("very fast cat")).get
-    val iter2 = PositionalIter.exact(index, Query.Phrase("day")).get
-    val iter = new AndIter(Array(iter1, iter2))
-    val docs = iter.takeWhile(_ > -1).toSet
-    assert(docs.isEmpty)
+  test("never match with 1 noMatch (last doc)") {
+    assert(alphaNumIndex.numDocs == 4, clue = "expected 4 to be last docId")
+    val queries = List(
+      TestQuery.exact("doc4", "0 b 2 d"),
+      TestQuery.noMatch,
+    )
+    checkAllPermutations(queries, List.empty)
+  }
+
+  test("always match with all matching, 1 query (doc1)") {
+    val queries = List(
+      TestQuery.exact("doc1", "0 1 2 3")
+    )
+    checkAllPermutations(queries, List(1))
+  }
+
+  test("always match with all matching, 1 query (last doc)") {
+    assert(alphaNumIndex.numDocs == 4, clue = "expected 4 to be last docId")
+    val queries = List(
+      TestQuery.exact("doc4", "0 b 2 d")
+    )
+    checkAllPermutations(queries, List(4))
+  }
+
+  test("always match with all matching, multiple queries (doc1)") {
+    val queries = List(
+      TestQuery.exact("doc1", "0 1 2 3"),
+      TestQuery.exact("doc1", "3 4 5 6"),
+      TestQuery.exact("doc1", "6 7 8 9"),
+    )
+    checkAllPermutations(queries, List(1))
+  }
+
+  test("never match when queries only match different docs") {
+    val queries = List(
+      TestQuery.exact("doc1", "0 1 2 3"),
+      TestQuery.exact("doc1", "3 4 5 6"),
+      TestQuery.exact("doc2", "a b c d"),
+    )
+    checkAllPermutations(queries, List.empty)
+  }
+
+  test("always match with all matching, multiple queries, multiple docs") {
+    val queries = List(
+      TestQuery.exact("doc1&3", "1"),
+      TestQuery.exact("doc1&3", "3"),
+    )
+    checkAllPermutations(queries, List(1, 3))
   }
 }
